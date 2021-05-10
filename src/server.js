2e9e290c839aa47a3bdcf97762e13cbd14b66cd3
gerica <rogeriogerica@gmail.com>
2021-05-10 18:55:27 -0300

    env

--- src/server.js
+++ src/server.js
@@ -8,7 +8,10 @@ import { importSchema } from 'graphql-import';
 import logger, { loggerConfig } from './utils/logger';
 import resolvers from './graphql/resolvers/index';
 
-dotenv.config();
+if (process.env.NODE_ENV !== 'production') {
+  dotenv.config();
+}
+
 const { PORT, HOST, PATH_GRAPHQL } = process.env;
 logger.info(`Your config: host:${HOST} port:${PORT} path:${PATH_GRAPHQL}`); // 8626
 
@@ -32,7 +35,3 @@ app.use(
 );
 app.listen(PORT);
 logger.info(`Running a GraphQL API server at http://${HOST}:${PORT}/graphql`);
-// const user = { firstName: 'Maxim', lastName: 'Orlov' };
-// logger.info(user, 'User successfully authenticated');
-// const error = new Error('Database crashed!');
-// logger.error(error, 'Failed to fetch user');
