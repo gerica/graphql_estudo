aa0bce326cdb50250752b75dfbdf6ffaede0e788
gerica <rogeriogerica@gmail.com>
2021-05-10 19:15:58 -0300

    config

--- src/server.js
+++ src/server.js
@@ -1,19 +1,14 @@
 import express from 'express';
-import dotenv from 'dotenv';
 
 import { graphqlHTTP } from 'express-graphql';
 import { buildSchema } from 'graphql';
 import { importSchema } from 'graphql-import';
-
+import config from './config/config';
 import logger, { loggerConfig } from './utils/logger';
 import resolvers from './graphql/resolvers/index';
 
-if (process.env.NODE_ENV !== 'production') {
-  dotenv.config();
-}
-
-const { PORT, HOST, PATH_GRAPHQL } = process.env;
-logger.info(`Your config: host:${HOST} port:${PORT} path:${PATH_GRAPHQL}`); // 8626
+const { host, port, endpoint } = config;
+logger.info(config, 'Your config:');
 
 const loggingMiddleware = (req, res, next) => {
   logger.debug(`ip: ${req.ip}`);
@@ -26,12 +21,12 @@ app.use(loggingMiddleware);
 app.use(loggerConfig);
 
 app.use(
-  PATH_GRAPHQL,
+  endpoint,
   graphqlHTTP({
     schema: buildSchema(importSchema('**/*.gql')),
     rootValue: resolvers,
     graphiql: true,
   }),
 );
-app.listen(PORT);
-logger.info(`Running a GraphQL API server at http://${HOST}:${PORT}/graphql`);
+app.listen(port);
+logger.info(`Running a GraphQL API server at http://${host}:${port}/graphql`);
